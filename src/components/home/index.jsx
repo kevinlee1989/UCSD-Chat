cdc2b030c69d1e8470c4f862039dcc67dd0fae18
KingMedieval <tshimada@purdue.edu>
2024-10-10 14:36:57 -0700

    push

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useAuth } from '../../contexts/authContext'
 
 const Home = () => {
-    const { currentUser } = useAuth()
+    const { currentUser } = useAuth();
     return (
         <div className='text-2xl font-bold pt-14'>Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.</div>
     )
