32c5690627e42e71804d7a2857e23ea360fddb1d
kevinlee1989 <kjmin99999@gmail.com>
2024-10-22 15:56:49 -0700

    kevin login service

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -46,4 +46,64 @@ router.post('/signup', async (req, res) => {
     }
 });
 
+/*
+example
+
+{
+  "uid": "R2lQRFIwa3RJMgJUawGvYGmNM163",
+  "email" : "bbeat2782@gmail.com",
+  "id" : ["course1", "course2", "course3", "course4"]
+}
+
+response
+
+{
+  "message": "Course IDs for user bbeat2782@gmail.com",
+  "ids": ["course1", "course2", "course3", "course4"]
+}
+
+*/ 
+router.get('/login', async (req, res) => {
+
+    // users Schema
+    const UserSchema = new mongoose.Schema({
+        uid: String,
+        email: String,
+        id: [String]
+    });
+
+    const User = mongoose.model('User', UserSchema);
+    const Course = mongoose.model('Course', CourseSchema);
+
+
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).send('Invalid request: email are required.');
+    }
+
+    try{
+        const user = await User.findOne({ email })
+
+        // If there is no user's email
+        if(!user){
+            return res.status(404).send('User not found');
+        }
+
+        // response course id array
+        return res.status(200).json({
+            message: `Course IDs for user ${email}`,
+            ids: user.id
+          });
+
+        
+    }catch{ // error dectector
+        console.error('Error while fetching user:', error);
+        return res.status(500).send('Internal server error');
+    }
+
+})
+
+
+
 module.exports = router;
