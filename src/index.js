1792bda778d358142e9ac919ae0f2166704affaa
jinseo215 <jic089@ucsd.edu>
2024-10-22 17:11:04 -0700

    home

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   </BrowserRouter>
 );
