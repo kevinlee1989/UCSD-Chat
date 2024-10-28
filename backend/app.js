646d847217e285475daf6784403e612e57d54d53
tomo <tshimada@purdue.edu>
2024-10-27 20:07:28 -0700

    refactoring and annotation.

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
 var courseRouter = require('./routes/course');
-var wsEchoRouter = require('./routes/echo');
+var wsRouter = require('./routes/websocket');
 
 
 // view engine setup
@@ -32,7 +32,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 app.use('/course', courseRouter);
-app.use('/echo', wsEchoRouter);
+app.use('/echo', wsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/websocket.js
+++ backend/routes/websocket.js
@@ -1,17 +1,31 @@
 var express = require('express');
 const {connect} = require("net");
 var router = express.Router();
+
+//set to store all connections
 var connections = new Set();
 
 router.ws('/', function (ws, req) {
+
+    //on error, log the error
     ws.on('error', console.error);
+
+    //log the new connection
     console.log("New connection has opened!");
+
+    //get the user_id and name from the query params
     let { user_id, name } = req.query;
+
+    //add to the active connection set
     connections.add(ws);
+
+    //on close, log the close event
     ws.on('close', function() {
         console.log('The connection was closed!');
         connections.delete(ws);
     });
+
+    //on message, log the message and send it back to the all the active connections
     ws.on('message', function (msg) {
         //ws.send(msg + ' received from ' + name);
         broadcast(name + " sent: " + msg);
@@ -19,10 +33,13 @@ router.ws('/', function (ws, req) {
     });
 });
 
+//broadcast function to send message to all active connections
 const broadcast = (msg) => {
+
+    //loop through all the active connections
     connections.forEach((ws)=>{
         //if the connections are objects with info use something like ws.ws.send()
-        ws.send(msg)
+        ws.send(msg);
     })
 }
 module.exports = router;
